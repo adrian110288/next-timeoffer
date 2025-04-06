6debd0f57ac62866d61b7e18c7167b437e4d0155
Adrian Lesniak <adrian110288@protonmail.com>
2025-04-06 12:00:28 +0100

    Add company settings profile form

---
+++ src/lib/actions/admin-actions.ts
@@ -0,0 +1,60 @@
+"use server";
+import { auth } from "@clerk/nextjs/server";
+import { prisma } from "../prisma";
+import { revalidatePath } from "next/cache";
+
+export async function updateCompanyProfile({
+    name,
+    website,
+    logo,
+}: {
+    name: string;
+    website?: string;
+    logo?: string;
+}) {
+    try {
+        const { userId } = await auth();
+        if (!userId) {
+            throw new Error("Unauthorized");
+        }
+
+        const user = await prisma.user.findUnique({
+            where: {
+                clerkId: userId,
+            },
+            select: {
+                companyId: true,
+                role: true,
+            },
+        });
+
+        if (!user) {
+            throw new Error("User not found");
+        }
+
+        if (user.role !== "ADMIN") {
+            throw new Error("Unauthorized");
+        }
+
+        await prisma.company.update({
+            where: {
+                id: user.companyId,
+            },
+            data: {
+                name,
+                website,
+                logo,
+            },
+        });
+
+        revalidatePath("/admin/company-settings/profile");
+        revalidatePath("/admin/company-settings");
+
+        return {
+            success: true,
+        };
+    } catch (error) {
+        console.error(error);
+        throw new Error("Failed to update company profile");
+    }
+}
